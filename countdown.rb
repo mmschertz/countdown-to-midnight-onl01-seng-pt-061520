9808966d6402ef09c0ecc4470700e7c7077572c2
Megan Schertz <meganschertz@gmail.com>
2020-06-19 01:24:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(int)
+ until int == 0 
+  puts "#{int} SECOND(S)!"
+  int -= 1 
+ end
+ return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  sleep 5
+ until int == 0 
+  puts "#{int} SECOND(S)!"
+  int -= 1 
+ end
+ return "HAPPY NEW YEAR!"
 end
